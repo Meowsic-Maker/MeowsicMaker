f9ed10a30c5f5f7068f06dd8eabf8a34d21dfa75
Iris Hsu <iris.shiotsuki@gmail.com>
2021-08-19 14:18:14 -0700

    updated zone boxes

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -19,45 +19,45 @@ export default class Game extends Phaser.Scene {
     let self = this;
 
     this.dealCatText = this.add
-      .text(75, 350, ["DEAL CATS"])
+      .text(50, 20, ["DEAL CATS"])
       .setFontSize(18)
       .setFontFamily("Trebuchet MS")
       .setColor("#00ffff")
       .setInteractive();
 
     this.zone1 = new Zone(this);
-    this.dropZone1 = this.zone1.renderZone(660, 500, 100, 100);
+    this.dropZone1 = this.zone1.renderZone(645, 480, 210, 360);
     this.outline1 = this.zone1.renderOutline(this.dropZone1);
 
     this.zone2 = new Zone(this);
-    this.dropZone2 = this.zone2.renderZone(360, 400, 100, 100);
+    this.dropZone2 = this.zone2.renderZone(330, 400, 140, 240);
     this.outline2 = this.zone2.renderOutline(this.dropZone2);
 
     this.zone3 = new Zone(this);
-    this.dropZone3 = this.zone3.renderZone(460, 300, 100, 100);
+    this.dropZone3 = this.zone3.renderZone(470, 300, 140, 200);
     this.outline3 = this.zone3.renderOutline(this.dropZone3);
 
     this.zone4 = new Zone(this);
-    this.dropZone4 = this.zone4.renderZone(630, 190, 100, 100);
+    this.dropZone4 = this.zone4.renderZone(610, 200, 140, 200);
     this.outline4 = this.zone4.renderOutline(this.dropZone4);
 
     this.zone5 = new Zone(this);
-    this.dropZone5 = this.zone5.renderZone(740, 140, 100, 100);
+    this.dropZone5 = this.zone5.renderZone(740, 150, 120, 180);
     this.outline5 = this.zone5.renderOutline(this.dropZone5);
 
     this.zone6 = new Zone(this);
-    this.dropZone6 = this.zone6.renderZone(860, 290, 100, 100);
+    this.dropZone6 = this.zone6.renderZone(860, 305, 220, 130);
     this.outline6 = this.zone6.renderOutline(this.dropZone6);
 
     this.zone7 = new Zone(this);
-    this.dropZone7 = this.zone7.renderZone(950, 390, 100, 100);
+    this.dropZone7 = this.zone7.renderZone(920, 440, 220, 140);
     this.outline7 = this.zone7.renderOutline(this.dropZone7);
 
     this.dealCats = () => {
       this.dealCats = () => {
         for (let i = 0; i < 7; i++) {
           let playerCat = new Cat(this);
-          playerCat.render(475 + i * 100, 650, "cat");
+          playerCat.render(80, 100 + i * 100, "cat");
         }
       };
     };
